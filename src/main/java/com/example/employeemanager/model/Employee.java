0d78cde20acdc01490916f1fb8d84e2f575af39d
Aditya Vasisht <adityacvasisht@gmail.com>
2024-02-23 00:52:07 +0530

    'First-Commit'

---
+++ src/main/java/com/example/employeemanager/model/Employee.java
@@ -0,0 +1,84 @@
+package com.example.employeemanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobtitle() {
+        return jobtitle;
+    }
+
+    public void setJobtitle(String jobtitle) {
+        this.jobtitle = jobtitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getEmployeecode() {
+        return employeecode;
+    }
+
+    public void setEmployeecode(String employeecode) {
+        this.employeecode = employeecode;
+    }
+
+    private String email;
+
+    private String jobtitle;
+
+    private String phone;
+
+    private String imageurl;
+
+    private String employeecode;
+}
